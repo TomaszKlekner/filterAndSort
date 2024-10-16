b317351784cc5efd73643529575709a4ccf5a5f5
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-10-16 15:09:47 +0200

    Filter fetched data by unique countries and industries

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,42 @@ import Filters from "./components/Filters";
 import Sorting from "./components/Sorting";
 import "./styles/main.scss";
 import useFetchData from "./hooks/useFetchData";
+import { useState, useMemo, useEffect } from "react";
 
 const API_URL = "https://dujour.squiz.cloud/developer-challenge/data";
 
 function App() {
   const { data, loading, error } = useFetchData(API_URL);
+  const [countryList, setCountryList] = useState([]);
+  const [industryList, setIndustryList] = useState([]);
+  const [filters, setFilters] = useState({
+    country: "",
+    industry: "",
+  });
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const uniqueCountries = [...new Set(data.map((item) => item.country))];
+      const uniqueIndustries = [...new Set(data.map((item) => item.industry))];
+      setCountryList(uniqueCountries);
+      setIndustryList(uniqueIndustries);
+    }
+  }, [data]);
+
+  const filteredData = useMemo(() => {
+    let filteredList = data;
+    if (filters.country) {
+      filteredList = filteredList.filter((item) =>
+        item.country.includes(filters.country)
+      );
+    }
+    if (filters.industry) {
+      filteredList = filteredList.filter((item) =>
+        item.industry.includes(filters.industry)
+      );
+    }
+    return filteredList;
+  }, [data, filters]);
 
   return (
     <>
@@ -17,13 +48,18 @@ function App() {
       <main>
         <div className="container flex">
           <div className="sidebar">
-            <Filters />
+            <Filters
+              countryList={countryList}
+              industryList={industryList}
+              filters={filters}
+              setFilters={setFilters}
+            />
             <Sorting />
           </div>
           <div className="content">
             {loading && <p>Loading...</p>}
             {error && <p>Error fetching data</p>}
-            {!loading && !error && <DataTable data={data} />}
+            {!loading && !error && <DataTable data={filteredData} />}
           </div>
         </div>
       </main>
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,46 @@
-const Filters = () => {
-  return <div>Filters</div>;
+const Filters = ({ countryList, industryList, filters, setFilters }) => {
+  const handleFilterSelect = (event) => {
+    setFilters({
+      ...filters,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="filters">
+      <h2>Filters</h2>
+
+      <div className="filters__row">
+        <label htmlFor="country">Select a country</label>
+        <select
+          name="country"
+          value={filters.country}
+          id="country"
+          onChange={handleFilterSelect}
+        >
+          <option value="">All</option>
+          {countryList.map((country) => (
+            <option key={country}>{country}</option>
+          ))}
+        </select>
+      </div>
+
+      <div className="filters__row">
+        <label htmlFor="country">Select an industry</label>
+        <select
+          name="industry"
+          value={filters.industry}
+          id="industry"
+          onChange={handleFilterSelect}
+        >
+          <option value="">All</option>
+          {industryList.map((industry) => (
+            <option key={industry}>{industry}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
