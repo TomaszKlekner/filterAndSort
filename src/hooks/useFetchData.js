2804817e459ea1eb2f3f2d6d650f64cdc377b906
Tomasz Klekner <tomasz.klekner@gmail.com>
2024-10-18 13:45:20 +0200

    Add clean up function

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
@@ -7,18 +7,25 @@ const useFetchData = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       try {
         const response = await axios.get(url);
         setData(response.data);
         setLoading(false);
       } catch (err) {
+        if (err instanceof CanceledError) return;
         setError(err);
+        setLoading(false);
+      } finally {
         setLoading(false);
       }
     };
 
     fetchData();
+
+    return () => controller.abort();
   }, [url]);
 
   return { data, loading, error };
